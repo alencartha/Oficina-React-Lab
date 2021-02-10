a4289aca40f808a2c1547bc6fc81db8eac0c3f53
alencartha <alencar_tha@hotmail.com>
2021-02-10 15:31:20 -0300

    consumindo api pokemon

--- oficina/src/App.js
+++ oficina/src/App.js
@@ -1,28 +1,30 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-import User from './components/User.js';
 
-const users =[
-  {
-    name: 'Isa',
-    description:'Gosta de sorvete'
-  },
-  {
-    name: 'Monica',
-    description:'Gosta de mousse'
-  },
-  {
-    name: 'Cintia',
-    description:'Gosta de goiabada'
-  }
-];
 
 const App=() =>{
+
+  const [pokemons, setPokemon] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://pokeapi.co/api/v2/type/5')
+    .then(result => result.json())
+    .then(result =>{
+      setPokemon(result.pokemon)
+    })
+  },[])
+
+
   return (
     <div className="App">
-      <User name={users[0].name} description={users[0].description}/>  
-      <User name={users[1].name}  description={users[1].description} />  
-      <User name={users[2].name}  description={users[2].description}/>  
+    {
+      pokemons.map((item, indice) => {
+      return ( 
+        <p key={indice}>{item.pokemon.name}</p>
+      )
+    })
+  }
     </div>
   );
 }
